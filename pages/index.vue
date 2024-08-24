66baca80b78138c86845038ea98135f5fc96fca7
Arnab Maiti <arnabmaiti550@gmail.com>
2024-08-24 10:11:29 +0530

    promo

--- pages/index.vue
+++ pages/index.vue
@@ -14,21 +14,36 @@
         <li>D. Create a test and challenge the world.</li>
       </ol>
     </div>
+
+    <div class="video-container mt-8">
+      <iframe
+        width="100%"
+        src="https://www.youtube.com/embed/iiUyK07Sq3U?&autoplay=0"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <div class="mt-8 text-xl font-bold">
+      <a
+        href="https://students-community.com/post/66c62646d34ada79757c8a60/student's-community-features"
+        >Usage Guide >></a
+      >
+    </div>
     <div
-      class="mt-24 md:mt-28 rounded-sm border-2 border-cyan-800 bg-[#b3cce834] p-4"
+      class="mt-16 md:mt-16 rounded-sm border-2 border-cyan-800 bg-[#b3cce834] p-4"
     >
       <h3 class="text-xl font-bold text-cyan-700">Guidelines</h3>
       <ol type="1" class="font-semibold text-gray-500 leading-normal">
         <li>1. Be respectable.</li>
         <li>2. Use of inapproprite words is prohibited.</li>
         <li>3. Spam or promotional content is not permitted.</li>
-        <li>4. AI tools generated content is not allowed.</li>
+
         <li>
-          5. Posting sexual/explicit content will result in immediate account
+          4. Posting sexual/explicit content will result in immediate account
           termination.
         </li>
-        <li>6. Post must be related to the topic.</li>
-        <li>7. Be aware of copyright.</li>
+        <li>5. Post must be related to the topic.</li>
+        <li>6. Be aware of copyright.</li>
       </ol>
     </div>
     <span
@@ -56,11 +71,22 @@ watch(text, () => {
     reRender(); // Assuming reRender is defined somewhere
   });
 });
-onMounted(async () => {
-  const content = "<p>你好, мир! hi 😁😁😍</p>";
-  const encodedContent = utf8ToHex(content);
-  console.log("encoded ", encodedContent);
-  const decodedContent = hexToUtf8(encodedContent);
-  console.log("decoded ", decodedContent);
-});
+onMounted(async () => {});
 </script>
+<style scoped>
+.video-container {
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%; /* 16:9 aspect ratio (9/16 = 0.5625) */
+  height: 0;
+  overflow: hidden;
+}
+
+.video-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
